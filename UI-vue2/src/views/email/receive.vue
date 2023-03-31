a188d46a94715351b64eee91447faf31848cdb93
GALENS <260827400@qq.com>
2023-03-31 12:32:32 +0800

    feat: 添加老版本ui

---
+++ UI-vue2/src/views/email/receive.vue
@@ -0,0 +1,17 @@
+<template>
+  <EmailGroup class="email-group" groupType="receive" />
+</template>
+
+<script>
+import EmailGroup from './components/emailGroup.vue'
+
+export default {
+  components: { EmailGroup },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
